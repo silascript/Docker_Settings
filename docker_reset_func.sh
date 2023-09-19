5304d4e8fe70637d3d8e81ebc7d139178e237a43
silascript <silascript@outlook.com>
2023-09-19 22:17:24 +0800

    修改初始化函数脚本各种bug

--- docker_reset_func.sh
+++ docker_reset_func.sh
@@ -8,35 +8,36 @@
 # 创建 /etc/docker/ 目录
 function mkdir_etc_docker(){
 	
-	docker_ect_path=/etc/docker
+	docker_etc_path=/etc/docker
 
 	# 如果目录不存在则创建
-	if [ ! -d $docker_ect_path ];then
-		echo -e "\e[93m$docker_ect_path \e[96m目录不存在，现在创建....\n \e[0m"
+	if [ ! -d $docker_etc_path ];then
+		echo -e "\e[93m$docker_etc_path \e[96m目录不存在，现在创建....\n \e[0m"
 		# 创建目录
-		sudo mkdir -p $docker_ect_path
+		sudo mkdir -p $docker_etc_path
 
 		# 检测目录是否创建成功
 		if [ $? == 0 ];then
-			echo -e "\e[92m$docker_ect_path \e[96m创建成功！\n \e[0m"
+			echo -e "\e[92m$docker_etc_path \e[96m创建成功！\n \e[0m"
 		else
-			echo -e "\e[93m$docker_ect_path \e[96m创建失败！\n \e[0m"
+			echo -e "\e[93m$docker_etc_path \e[96m创建失败！\n \e[0m"
 		fi
 	else
-		echo -e "\e[92m$docker_ect_path \e[96m目录已存在！\n \e[0m"
+		echo -e "\e[92m$docker_etc_path \e[96m目录已存在！\n \e[0m"
 	fi
 
 }
 
+# 生成配置文件
+function generate_config(){
 
-# 设置镜像或代理
-function set_proxy(){
-
+	# 设置镜像或代理
 	sudo tee /etc/docker/daemon.json <<-'EOF'
 	{
 		"registry-mirrors": [
 			"https://dockerproxy.com",
 			"http://hub-mirror.c.163.com",
+			"https://mirror.baidubce.com/"
 		]
 	}
 	EOF
@@ -44,6 +45,38 @@ function set_proxy(){
 
 }
 
+# 重置
+function reset_docker(){
+	
+	docker_etc_path=/etc/docker
+	
+	# 停止 docker 服务
+	echo -e "\e[96m停止 docker 服务...\n \e[0m"
+	sudo systemctl stop docker
+	
+	echo -e "\e[96m开始重置docker...\n \e[0m"
+	# 如果存在 /etc/docker 目录将删除此目录
+	if [ -d $docker_etc_path ];then
+		echo -e "\e[96m开始删除 \e[92m$docker_etc_path \e[96m目录...\n \e[0m"
+		sudo rm -rf $docker_etc_path
+	else
+		echo -e "\e[92m$docker_etc_path \e[96m目录不存在，不用删了！\n \e[0m"
+	fi
+	
+	# 生成 /etc/docker 目录
+	echo -e "\e[96m重新生成 \e[92m $docker_etc_path \e[96m目录...\n \e[0m"
+	mkdir_etc_docker
+
+	# 生成 docker 配置文件
+	echo -e "\e[96m生成 \e[92m daemon.json \e[96m配置文件...\n \e[0m"
+	generate_config
+	
+
+	# 重启 docker 服务
+	echo -e "\e[96m启动 docker 服务...\n \e[0m"
+	sudo systemctl start docker
+
+}
 
 
 
@@ -58,7 +91,8 @@ function set_proxy(){
 
 # ---------------------测试-------------------- #
 
-mkdir_etc_docker
+# mkdir_etc_docker
 
+reset_docker
 
 
