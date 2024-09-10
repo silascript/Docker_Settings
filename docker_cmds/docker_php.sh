1b81eb75595ac35388bb06e2122e230307a84bbe
silascript <silascript@outlook.com>
2024-09-10 12:44:49 +0800

    修正docker php 脚本错误

--- docker_cmds/docker_php.sh
+++ docker_cmds/docker_php.sh
@@ -1,4 +1,4 @@
 # docker php 8.3
+cmd_str="/usr/local/bin/"
+docker exec -it d_php83 sh -c "$cmd_str/$*"
 # docker exec -it d_php83 sh -c "php $*"
-cmd_str="/usr/local/bin/*"
-docker exec -it d_php83 sh -c "$cmd_str $*"
